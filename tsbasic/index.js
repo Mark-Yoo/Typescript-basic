07ac2099da020cd6a5972ab910e2d2b5ef7a0a89
Mark Yoo <yjinhyuk@gmail.com>
2021-03-10 23:29:01 +0900

    Add: index.js, index.ts, tsconfig.js is now added Detail: typescript practice with compiler

---
+++ tsbasic/index.js
@@ -0,0 +1,4 @@
+function tsTest(a, b, c) {
+    return a + b + c;
+}
+console.log(tsTest(2, 3, '4'));
---
+++ tsbasic/index.ts
@@ -0,0 +1,5 @@
+function tsTest(a: number, b: number, c: string): string {
+  return a + b + c;
+}
+
+console.log(tsTest(2, 3, '4'));
