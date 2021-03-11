1d1c0a5e1c93a1ce4d08661a2bc63a4cb4881b0a
Mark Yoo <yjinhyuk@gmail.com>
2021-03-11 22:23:06 +0900

    Add: TIL-type-aliases.ts is now added Detail: Differences in type and interface

---
+++ TIL-Typescript/TIL-type-aliases.ts
@@ -0,0 +1,17 @@
+type User = {
+  id: string;
+  name: string;
+  age: number;
+}
+
+// interface User {
+//   id: string;
+//   name: string;
+//   age: number;
+// }
+
+let mark: User = {
+  id: "Mark-Yoo",
+  name: "Mark",
+  age: 29,
+}
